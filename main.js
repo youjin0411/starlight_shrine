3f5b5166f1e6958aa6599b2a81b7bb30996374bd
DESKTOP-1L7397I\82105 <w2117@e-mirim.hs.kr>
2022-09-19 02:09:44 +0900

    연혁

--- main.js
+++ main.js
@@ -16,4 +16,25 @@ const toggleMenu = (toggleId, navListId) => {
         });
     }
 }
-toggleMenu("nav-toggle", "nav-list");   
+toggleMenu("nav-toggle", "nav-list");
+
+
+"use strict";
+const content = "소중한 고객님들의 사연을 모아봤습니다."
+const text = document.querySelector(".text")
+let index = 0;
+
+ function sleep(delay){ 
+     const start = new Date().getTime(); 
+     while (new Date().getTime() < start + delay); 
+    }
+
+function typing(){
+    text.textContent += content[index++];
+    if(index > content.length){
+        text.textContent = ""
+        index = 0;
+        sleep(3000);
+    }
+}
+setInterval(typing, 200)

a9818c48abb6545639277bd0ca77e5cafa8e8326
DESKTOP-1L7397I\82105 <w2117@e-mirim.hs.kr>
2022-09-23 04:06:07 +0900

    Tip

--- Tip 페이지/main.js
+++ Tip 페이지/main.js
@@ -39,3 +39,22 @@ const boxList = document.querySelectorAll('.tip-item');
 
 // 반복문을 돌려 모든 DOM에 적용
 boxList.forEach(el => observer.observe(el));
+
+var target = document.querySelectorAll('.btn_open');
+var btnPopClose = document.querySelectorAll('.pop_wrap .btn_close');
+var targetID;
+
+// 팝업 열기
+for(var i = 0; i < target.length; i++){
+  target[i].addEventListener('click', function(){
+    targetID = this.getAttribute('href');
+    document.querySelector(targetID).style.display = 'block';
+  });
+}
+
+// 팝업 닫기
+for(var j = 0; j < target.length; j++){
+  btnPopClose[j].addEventListener('click', function(){
+    this.parentNode.parentNode.style.display = 'none';
+  });
+}

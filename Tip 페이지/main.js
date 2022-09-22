ec89288336af0cf3547122f006abc12a48b10285
DESKTOP-1L7397I\82105 <w2117@e-mirim.hs.kr>
2022-09-22 21:18:53 +0900

    Tip nav바 반응형 수정중

--- Tip 페이지/main.js
+++ Tip 페이지/main.js
@@ -17,3 +17,25 @@ const toggleMenu = (toggleId, navListId) => {
     }
 }
 toggleMenu("nav-toggle", "nav-list");
+
+
+const options = {
+  root: null, // viewport
+  rootMargin: "0px",
+  threshold: 0.5,  // 50%가 viewport에 들어와 있어야 callback 실행
+}
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+    } else {
+      entry.target.classList.remove('active');
+    }
+  });
+}, options);
+
+const boxList = document.querySelectorAll('.tip-item');
+
+// 반복문을 돌려 모든 DOM에 적용
+boxList.forEach(el => observer.observe(el));
